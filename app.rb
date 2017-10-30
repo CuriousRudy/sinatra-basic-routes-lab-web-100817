a9836557b9920ab1abbb3f6088524015c4181e51
Abe Rudy <aberudy@gmail.com>
2017-10-30 10:01:11 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,25 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    "Hello World!"
+  end
+
+  get '/name' do
+    "My name is Abe"
+  end
+
+
+  get '/hometown' do
+    "My hometown is Brooklyn, NY"
+  end
+
+
+  get '/favorite-song' do
+    "My favorite song is 451 by Brand New"
+  end
+
+
+
 end
